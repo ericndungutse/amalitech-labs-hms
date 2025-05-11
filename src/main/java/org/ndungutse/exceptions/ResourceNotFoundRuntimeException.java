42375e22d46060c968140f05d6e219fdd458a509
Eric Tuyizere Ndungutse <dav.ndungutse@gmail.com>
2025-05-11 16:45:07 +0200

    feat: handle jdbc exceptions and logging (#2)

---
+++ src/main/java/org/ndungutse/exceptions/ResourceNotFoundRuntimeException.java
@@ -0,0 +1,7 @@
+package org.ndungutse.exceptions;
+
+public class ResourceNotFoundRuntimeException extends RuntimeException {
+        public ResourceNotFoundRuntimeException(String message) {
+            super(message);
+        }
+}
